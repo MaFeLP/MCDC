e3822545d660d5f16a4511f34e6a8e6fdc7abdb1
GitHub <maxfehlinger@gmx.de>
2021-03-26 14:29:05 +0100

    🏁 Small bug fixes --- 🛠 Updated Config.java: ↳ Added subcommands set/get/add/remove to the /config command. ↳ Updated plugin.yml to represent those changes in the usages part. 📗 Updated the javadoc --- moved to version 0.6.3-beta

--- src/main/java/com/github/mafelp/utils/Settings.java
+++ src/main/java/com/github/mafelp/utils/Settings.java
@@ -35,7 +35,7 @@ public class Settings {
     /**
      * version number of the plugin - displayed to users
      */
-    public static final String version = "v0.6.2-beta";
+    public static final String version = "v0.6.3-beta";
 
     /**
      * enables more information being displayed while executing events
