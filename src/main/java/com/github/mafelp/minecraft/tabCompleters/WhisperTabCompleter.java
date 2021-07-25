beb51fcb54c6414d1757054472b3c5bc0e3b5ad1
GitHub <60669873+MaFeLP@users.noreply.github.com>
2021-07-25 18:48:44 +0200

    📗 Added Javadoc to the WhisperTabCompleter

--- src/main/java/com/github/mafelp/minecraft/tabCompleters/WhisperTabCompleter.java
+++ src/main/java/com/github/mafelp/minecraft/tabCompleters/WhisperTabCompleter.java
@@ -13,7 +13,18 @@ import java.util.List;
 
 import static com.github.mafelp.minecraft.tabCompleters.ResultSorter.sortedResults;
 
+/**
+ * The method that handles all the tab completion for the /whisper and /dcmsg commands.
+ */
 public class WhisperTabCompleter implements TabCompleter {
+    /**
+     * The Method that handles the getting of of the results.
+     * @param sender The sender that is typing the command.
+     * @param cmd The command that the sender is trying to type.
+     * @param alias The first argument of the command, which also can be an alias.
+     * @param args The arguments that are provided until now.
+     * @return A list of available results for this command.
+     */
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String alias, @NotNull String[] args) {
         final List<String> results = new ArrayList<>();
