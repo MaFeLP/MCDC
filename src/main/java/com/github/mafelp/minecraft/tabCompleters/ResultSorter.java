0752c875fef55dd484cef8a1b375164a1b2e747b
GitHub <60669873+MaFeLP@users.noreply.github.com>
2021-07-25 18:40:43 +0200

    📗 Added Javadoc to the ResultSorter

--- src/main/java/com/github/mafelp/minecraft/tabCompleters/ResultSorter.java
+++ src/main/java/com/github/mafelp/minecraft/tabCompleters/ResultSorter.java
@@ -6,7 +6,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A class that sorts tab completion results.
+ */
 public class ResultSorter {
+    /**
+     * A method that sorts the results provided in the second argument alphabetically and after what has already been
+     * typed (first argument).
+     * @param arg The string that has already been typed.
+     * @param results The results to be sorted.
+     * @return The sorted results.
+     */
     // Sorts possible results to provide true tab auto complete based off of what is already typed.
     public static List<String> sortedResults(String arg, List<String> results) {
         final List<String> completions = new ArrayList<>();
