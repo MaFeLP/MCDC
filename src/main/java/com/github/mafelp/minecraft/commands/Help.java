c4edb029c188eb650f8cfee4d66c75f05eab1073
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2021-08-10 19:55:02 +0200

    ➕ Added help for the help command

--- src/main/java/com/github/mafelp/minecraft/commands/Help.java
+++ src/main/java/com/github/mafelp/minecraft/commands/Help.java
@@ -14,17 +14,18 @@ import java.util.Locale;
 import static com.github.mafelp.utils.Logging.info;
 
 public class Help implements CommandExecutor {
+    private static final String spacerLine = ChatColor.GREEN + "+--------------------" + ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "MCDC" + ChatColor.DARK_GRAY + "/" + ChatColor.YELLOW + "HELP" + ChatColor.DARK_GRAY + "]" + ChatColor.GREEN + "--------------------+\n";
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        String out = ChatColor.GREEN + "+--------------------" + ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "MCDC" + ChatColor.DARK_GRAY + "/" + ChatColor.YELLOW + "HELP" + ChatColor.DARK_GRAY + "]" + ChatColor.GREEN + "--------------------+\n";
+        String out = spacerLine + "";
 
         if (args.length == 0) {
             out += normalHelpMessage(sender);
             info("\"" + sender.getName() + "\" executed command \"/help\"; Result: Normal help message.");
         } else {
             switch (args[0].toLowerCase(Locale.ROOT)) {
-                case "account" -> {
-                }
+                case "account" -> out += accountHelp(sender instanceof Player, CheckPermission.checkPermission(Permissions.accountEdit, sender));
                 case "config" -> {
                 }
                 case "help" -> {
@@ -45,7 +46,7 @@ public class Help implements CommandExecutor {
             }
         }
 
-        out += ChatColor.GREEN + "+--------------------" + ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "MCDC" + ChatColor.DARK_GRAY + "/" + ChatColor.YELLOW + "HELP" + ChatColor.DARK_GRAY + "]" + ChatColor.GREEN + "--------------------+";
+        out += spacerLine;
         sender.sendMessage(out);
 
         return true;
@@ -105,6 +106,54 @@ public class Help implements CommandExecutor {
             out += ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "/unlink" + ChatColor.GRAY + "      -> " + ChatColor.BLUE + "Removes your account\n";
 
         out += ChatColor.GRAY + " `-> " + ChatColor.DARK_AQUA + "/whisper" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Sends a private message to a discord user\n";
+
+        return out;
+    }
+
+    private static String commandHelpPageStarter(String description) {
+        return ChatColor.RED + " If you are searching for the vanilla help, use \"" + ChatColor.GRAY + "/minecraft:help" + ChatColor.RED + "\"!\n\n"
+                + ChatColor.AQUA + " Help for command \"" + ChatColor.GRAY + "/account" + ChatColor.AQUA + "\". Use \"" + ChatColor.GRAY + "/mcdc:help" + ChatColor.AQUA + "\"\n"
+                + " to see a list of all available commands.\n\n"
+                + ChatColor.GREEN + ChatColor.UNDERLINE + "Description" + ChatColor.RESET + ChatColor.DARK_GRAY + ":\n"
+                + ChatColor.RESET + description + "\n"
+                + ChatColor.RESET + " The subcommands are structured as followed:\n"
+                + ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "subcommand 1" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Description\n"
+                + ChatColor.GRAY + " | |-> " + ChatColor.DARK_AQUA + "subcommand argument 1" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Description\n"
+                + ChatColor.GRAY + " | `-> " + ChatColor.DARK_AQUA + "subcommand argument 2" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Description\n"
+                + ChatColor.GRAY + " `-> " + ChatColor.DARK_AQUA + "subcommand 2" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Description\n"
+                + ChatColor.GRAY + "   |-> " + ChatColor.DARK_AQUA + "subcommand argument 1" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Description\n"
+                + ChatColor.GRAY + "   `-> " + ChatColor.DARK_AQUA + "subcommand argument 2" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Description\n"
+                + spacerLine
+                + ChatColor.GREEN + ChatColor.UNDERLINE + "Subcommands" + ChatColor.RESET + ChatColor.DARK_GRAY + ":\n";
+    }
+
+    private static String accountHelp(boolean isPlayer, boolean hasAccountEditPermission) {
+        String out = commandHelpPageStarter(" The account command provides lots of useful features for managing\n"
+                + " your local minecraft server/discord relationship.\n"
+                + ChatColor.RED + ChatColor.BOLD + " THIS ACCOUNT IS NOT OFFICIAL AND ONLY VALID ON THIS SERVER AND WITH THE MCDC BOT!")
+                ;
+
+        if (isPlayer)
+            out += ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "link" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Initiates the mc/dc linking process.\n"
+                 + ChatColor.GRAY + " | `-> " + ChatColor.DARK_AQUA + "token" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "OPTIONAL - the linking token, given from discord\n"
+                 + ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "name" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Give yourself a new account tag\n"
+                 + ChatColor.GRAY + " | `-> " + ChatColor.DARK_AQUA + "name" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "STRING - your new account name\n"
+                 + ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "username" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Give yourself a new account tag\n"
+                 + ChatColor.GRAY + " | `-> " + ChatColor.DARK_AQUA + "name" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "STRING - your new account name\n"
+                    ;
+
+        out += ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "get" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Gets the account tag for a user\n"
+            + ChatColor.GRAY + " | `-> " + ChatColor.DARK_AQUA + "user" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "The user to get the tag from\n";
+
+        if (hasAccountEditPermission)
+            out += ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "remove" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Removes an account from a user\n"
+                    + ChatColor.GRAY + " | `-> " + ChatColor.DARK_AQUA + "account tag" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "The account tag of the user whose account shall be removed.\n"
+                    + ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "save" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Saves all currently linked accounts to the file.\n"
+                    + ChatColor.GRAY + " |-> " + ChatColor.DARK_AQUA + "reload" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Reloads all accounts from the file.\n"
+                    ;
+
+        if (isPlayer)
+            out += ChatColor.GRAY + " `-> " + ChatColor.DARK_AQUA + "unlink" + ChatColor.GRAY + "   -> " + ChatColor.BLUE + "Deletes your account\n";
 
         return out;
     }
