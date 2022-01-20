e8188a2ebde837d56dc5f4900880035607c3b227
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2022-01-20 23:08:49 +0100

    🆙 Bumped Project Version number to 0.12.0-beta
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- src/main/java/com/github/mafelp/utils/Settings.java
+++ src/main/java/com/github/mafelp/utils/Settings.java
@@ -36,7 +36,7 @@ public class Settings {
     /**
      * version number of the plugin - displayed to users
      */
-    public static final String version = "v0.11.0-beta";
+    public static final String version = "v0.12.0-beta";
 
     /**
      * enables more information being displayed while executing events
